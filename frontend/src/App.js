79a76b547caeccbdee7d38251ca1e351cb4b6b46
Franklin Bolivar <frank.uah@gmail.com>
2022-05-02 14:43:42 -0400

    iniciando aun

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,12 @@ import ProveedorListScreen from './screens/ProveedorListScreen';
 import ProveedorEditScreen from './screens/ProveedorEditScreen';
 import ProveedorRegisterScreen from './screens/ProveedorRegisterScreen';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faCartArrowDown, faUser, faUserCog } from '@fortawesome/free-solid-svg-icons';
+import {
+	faCaretDown,
+	faCartArrowDown,
+	faUser,
+	faUserCog,
+} from '@fortawesome/free-solid-svg-icons';
 import { faUserShield } from '@fortawesome/free-solid-svg-icons';
 import Tooltip from './components/Tooltip';
 import { ToastContainer } from 'react-toastify';
@@ -55,279 +60,315 @@ import DashboardScreen from './screens/DashboardScreen';
  */
 
 function App() {
-  const cart = useSelector(state => state.cart);
-  const { cartItems } = cart;
-  const userSignin = useSelector(state => state.userSignin);
-  const { userInfo } = userSignin;
-  const dispatch = useDispatch();
-  const signoutHandler = () => {
-    dispatch(signout());
-  };
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
+	const dispatch = useDispatch();
+	const signoutHandler = () => {
+		dispatch(signout());
+	};
 
-  return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
-          <div className="row">
-            <Link to="/">
-              <img className="logo" src={logo} alt="logo" />
-            </Link>
-            <Link className="brand" to="/">
-              Automotores La Cima, C.A.
-            </Link>
-          </div>
-          <div className="row">
-            {userInfo ? (
-              <div className="dropdown">
-                <Link className="link-usuario" to="#">
-                  {userInfo.nombre.substr(0, 12)}
-                  <FontAwesomeIcon icon={faCaretDown} />
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/profile">Mi Perfil</Link>
-                  </li>
-                  <li>
-                    <Link to="/orderhistory">Mis Pedidos</Link>
-                  </li>
-                  <li>
-                    <Link to="#signout" onClick={signoutHandler}>
-                      Cerrar Sesion
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to="/signin">
-                <Tooltip position="left" content="Iniciar Sesion">
-                  <FontAwesomeIcon icon={faUser} />
-                </Tooltip>
-              </Link>
-            )}
+	return (
+		<BrowserRouter>
+			<div className='grid-container'>
+				<header className='row'>
+					<div className='row'>
+						<Link to='/'>
+							<img className='logo' src={logo} alt='logo' />
+						</Link>
+						<Link className='brand' to='/'>
+							Transacc-App
+						</Link>
+					</div>
+					<div className='row'>
+						{userInfo ? (
+							<div className='dropdown'>
+								<Link className='link-usuario' to='#'>
+									{userInfo.nombre.substr(0, 12)}
+									<FontAwesomeIcon icon={faCaretDown} />
+								</Link>
+								<ul className='dropdown-content'>
+									<li>
+										<Link to='/profile'>Mi Perfil</Link>
+									</li>
+									<li>
+										<Link to='/orderhistory'>Mis Pedidos</Link>
+									</li>
+									<li>
+										<Link to='#signout' onClick={signoutHandler}>
+											Cerrar Sesion
+										</Link>
+									</li>
+								</ul>
+							</div>
+						) : (
+							<Link to='/signin'>
+								<Tooltip position='left' content='Iniciar Sesion'>
+									<FontAwesomeIcon icon={faUser} />
+								</Tooltip>
+							</Link>
+						)}
 
-            {userInfo && userInfo.isAdmin && (
-              <Link to="/administrador">
-                <Tooltip position="left" content="Administrador">
-                  <FontAwesomeIcon icon={faUserShield} />
-                </Tooltip>
-              </Link>
-            )}
-            {userInfo && userInfo.isVendedor && <FontAwesomeIcon icon={faUserCog} />}
-            <Link to="/cart">
-              <Tooltip position="left" content="Pedido Actual">
-                <FontAwesomeIcon icon={faCartArrowDown} />
-              </Tooltip>
-              {cartItems.length > 0 && <span className="badge">{cartItems.length}</span>}
-            </Link>
-          </div>
-        </header>
-        <main>
-          <ToastContainer limit={1} />
-          <Routes>
-            <Route path="/administrador" element={<AdminScreen />}></Route>
-            <Route path="/dashboard" element={<DashboardScreen />}></Route>
-            <Route path="/cart" element={<CartScreen />}></Route>
-            <Route path="/cart/:id" element={<CartScreen />}></Route>
-            <Route path="/product/:id" element={<ProductScreen />} exact></Route>
-            <Route path="/product/:id/edit" element={<ProductEditScreen />} exact></Route>
-            <Route path="/entrada" element={<NotaEntradaScreen />}></Route>
-            <Route path="/signin" element={<SigninScreen />}></Route>
-            <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
-            <Route path="/clienteinfo" element={<ClienteInfoScreen />}></Route>
-            <Route path="/clientefastregister" element={<ClienteFastRegisterScreen />}></Route>
-            <Route path="/payment" element={<PaymentMethodScreen />}></Route>
-            <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
-            <Route path="/placecotizacion" element={<PlaceCotizacionScreen />}></Route>
-            <Route path="/order/:id" element={<OrderScreen />}></Route>
-            <Route path="/printorder/:id" element={<PrintOrderScreen />} exact></Route>
-            <Route path="/cotizacion/:id" element={<CotizacionScreen />}></Route>
-            <Route path="/printcotizacion/:id" element={<PrintCotizacionScreen />} exact></Route>
-            <Route path="/entrada/:id" element={<EntradaView />}></Route>
-            <Route path="/orderhistory" element={<OrderHistoryScreen />}></Route>
+						{userInfo && userInfo.isAdmin && (
+							<Link to='/administrador'>
+								<Tooltip position='left' content='Administrador'>
+									<FontAwesomeIcon icon={faUserShield} />
+								</Tooltip>
+							</Link>
+						)}
+						{userInfo && userInfo.isVendedor && (
+							<FontAwesomeIcon icon={faUserCog} />
+						)}
+						<Link to='/cart'>
+							<Tooltip position='left' content='Pedido Actual'>
+								<FontAwesomeIcon icon={faCartArrowDown} />
+							</Tooltip>
+							{cartItems.length > 0 && (
+								<span className='badge'>{cartItems.length}</span>
+							)}
+						</Link>
+					</div>
+				</header>
+				<main>
+					<ToastContainer limit={1} />
+					<Routes>
+						<Route path='/administrador' element={<AdminScreen />}></Route>
+						<Route path='/dashboard' element={<DashboardScreen />}></Route>
+						<Route path='/cart' element={<CartScreen />}></Route>
+						<Route path='/cart/:id' element={<CartScreen />}></Route>
+						<Route
+							path='/product/:id'
+							element={<ProductScreen />}
+							exact
+						></Route>
+						<Route
+							path='/product/:id/edit'
+							element={<ProductEditScreen />}
+							exact
+						></Route>
+						<Route path='/entrada' element={<NotaEntradaScreen />}></Route>
+						<Route path='/signin' element={<SigninScreen />}></Route>
+						<Route path='/shipping' element={<ShippingAddressScreen />}></Route>
+						<Route path='/clienteinfo' element={<ClienteInfoScreen />}></Route>
+						<Route
+							path='/clientefastregister'
+							element={<ClienteFastRegisterScreen />}
+						></Route>
+						<Route path='/payment' element={<PaymentMethodScreen />}></Route>
+						<Route path='/placeorder' element={<PlaceOrderScreen />}></Route>
+						<Route
+							path='/placecotizacion'
+							element={<PlaceCotizacionScreen />}
+						></Route>
+						<Route path='/order/:id' element={<OrderScreen />}></Route>
+						<Route
+							path='/printorder/:id'
+							element={<PrintOrderScreen />}
+							exact
+						></Route>
+						<Route
+							path='/cotizacion/:id'
+							element={<CotizacionScreen />}
+						></Route>
+						<Route
+							path='/printcotizacion/:id'
+							element={<PrintCotizacionScreen />}
+							exact
+						></Route>
+						<Route path='/entrada/:id' element={<EntradaView />}></Route>
+						<Route
+							path='/orderhistory'
+							element={<OrderHistoryScreen />}
+						></Route>
 
-            <Route path="/register" element={<RegisterScreen />}></Route>
+						<Route path='/register' element={<RegisterScreen />}></Route>
 
-            <Route
-              path="/profile"
-              element={
-                <PrivateRoute>
-                  <ProfileScreen />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/productlist"
-              element={
-                <AdminRoute>
-                  <ProductListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/profile'
+							element={
+								<PrivateRoute>
+									<ProfileScreen />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path='/productlist'
+							element={
+								<AdminRoute>
+									<ProductListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/clientelist"
-              element={
-                <AdminRoute>
-                  <ClienteListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
-            <Route
-              path="/proveedorlist"
-              element={
-                <AdminRoute>
-                  <ProveedorListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
-            <Route
-              path="/createproduct"
-              element={
-                <AdminRoute>
-                  <ProductCreateScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/clientelist'
+							element={
+								<AdminRoute>
+									<ClienteListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
+						<Route
+							path='/proveedorlist'
+							element={
+								<AdminRoute>
+									<ProveedorListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
+						<Route
+							path='/createproduct'
+							element={
+								<AdminRoute>
+									<ProductCreateScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/settings/:id"
-              element={
-                <AdminRoute>
-                  <CambioDiaScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/settings/:id'
+							element={
+								<AdminRoute>
+									<CambioDiaScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/registrarcliente"
-              element={
-                <AdminRoute>
-                  <ClienteRegisterScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/registrarcliente'
+							element={
+								<AdminRoute>
+									<ClienteRegisterScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/registrarproveedor"
-              element={
-                <AdminRoute>
-                  <ProveedorRegisterScreen />
-                </AdminRoute>
-              }
-              exact
-            />
-            <Route
-              path="/productlist/pageNumber/:pageNumber"
-              element={
-                <AdminRoute>
-                  <ProductListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
-            <Route
-              path="/orderlist"
-              element={
-                <AdminRoute>
-                  <OrderListScreen />
-                </AdminRoute>
-              }
-            />
+						<Route
+							path='/registrarproveedor'
+							element={
+								<AdminRoute>
+									<ProveedorRegisterScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
+						<Route
+							path='/productlist/pageNumber/:pageNumber'
+							element={
+								<AdminRoute>
+									<ProductListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
+						<Route
+							path='/orderlist'
+							element={
+								<AdminRoute>
+									<OrderListScreen />
+								</AdminRoute>
+							}
+						/>
 
-            <Route
-              path="/orderlist/pageNumber/:pageNumber"
-              element={
-                <AdminRoute>
-                  <OrderListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/orderlist/pageNumber/:pageNumber'
+							element={
+								<AdminRoute>
+									<OrderListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/cotizacionlist"
-              element={
-                <AdminRoute>
-                  <CotizacionListScreen />
-                </AdminRoute>
-              }
-            />
+						<Route
+							path='/cotizacionlist'
+							element={
+								<AdminRoute>
+									<CotizacionListScreen />
+								</AdminRoute>
+							}
+						/>
 
-            <Route
-              path="/cotizacionlist/pageNumber/:pageNumber"
-              element={
-                <AdminRoute>
-                  <CotizacionListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/cotizacionlist/pageNumber/:pageNumber'
+							element={
+								<AdminRoute>
+									<CotizacionListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/entradalist"
-              element={
-                <AdminRoute>
-                  <EntradaListScreen />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/userlist"
-              element={
-                <AdminRoute>
-                  <UserListScreen />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/userlist/pageNumber/:pageNumber"
-              element={
-                <AdminRoute>
-                  <UserListScreen />
-                </AdminRoute>
-              }
-              exact
-            />
+						<Route
+							path='/entradalist'
+							element={
+								<AdminRoute>
+									<EntradaListScreen />
+								</AdminRoute>
+							}
+						/>
+						<Route
+							path='/userlist'
+							element={
+								<AdminRoute>
+									<UserListScreen />
+								</AdminRoute>
+							}
+						/>
+						<Route
+							path='/userlist/pageNumber/:pageNumber'
+							element={
+								<AdminRoute>
+									<UserListScreen />
+								</AdminRoute>
+							}
+							exact
+						/>
 
-            <Route
-              path="/user/:id/edit"
-              element={
-                <AdminRoute>
-                  <UserEditScreen />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/cliente/:id/edit"
-              element={
-                <AdminRoute>
-                  <ClienteEditScreen />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/proveedor/:id/edit"
-              element={
-                <AdminRoute>
-                  <ProveedorEditScreen />
-                </AdminRoute>
-              }
-            />
+						<Route
+							path='/user/:id/edit'
+							element={
+								<AdminRoute>
+									<UserEditScreen />
+								</AdminRoute>
+							}
+						/>
+						<Route
+							path='/cliente/:id/edit'
+							element={
+								<AdminRoute>
+									<ClienteEditScreen />
+								</AdminRoute>
+							}
+						/>
+						<Route
+							path='/proveedor/:id/edit'
+							element={
+								<AdminRoute>
+									<ProveedorEditScreen />
+								</AdminRoute>
+							}
+						/>
 
-            <Route path="/pageNumber/:pageNumber" element={<HomeScreen />} exact></Route>
-            <Route path="/" element={<HomeScreen />} exact></Route>
-          </Routes>
-        </main>
-        <footer className="row center">LaCima-Stock - v1.0 01mayo2022</footer>
-      </div>
-    </BrowserRouter>
-  );
+						<Route
+							path='/pageNumber/:pageNumber'
+							element={<HomeScreen />}
+							exact
+						></Route>
+						<Route path='/' element={<HomeScreen />} exact></Route>
+					</Routes>
+				</main>
+				<footer className='row center'>LaCima-Stock - v1.0 01mayo2022</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
